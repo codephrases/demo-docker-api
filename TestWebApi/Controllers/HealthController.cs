a2d98c682f8b7f12715f040137cbbff8cc22cc24
Muhammad Usman <living9to5@gmail.com>
2022-08-26 21:01:13 +0500

    First code commit

---
+++ TestWebApi/Controllers/HealthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Controllers {
+
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase {
+
+        private readonly ILogger<HealthController> logger;
+
+        public HealthController(ILogger<HealthController> logger) {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Health() {
+            return Ok("healthy");
+        }
+    }
+}
